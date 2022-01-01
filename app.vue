6470c6ff339c51b7f7116a14d52751adaaffb0ce
Nicole J. Nobles <shecodez@gmail.com>
2022-01-01 09:27:33 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Audiowide&display=swap");
+
+.app {
+  height: 100%;
+}
+
+.bg-0d0d0d {
+  background-color: #0d0d0d;
+}
+
+.font-brand {
+  font-family: "Audiowide", cursive;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout name="landing">
+    <div class="w-full h-full xl:w-1/2 flex items-center mx-auto">
+      <img src="/assets/under-construction.png" alt="under construction" />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script>
+export default {
+  layout: false,
+};
+</script>
